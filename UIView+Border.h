0a004572de9326bfdd1b89d1820aba4c3ded4a2a
Yubin <lion_enginner@163.com>
2016-12-26 17:26:06 +0800

    上传给UIView添加边框的类文件

---
+++ UIView+Border.h
@@ -0,0 +1,88 @@
+//
+//  UIView+Border.h
+//  Layout
+//
+//  Created by Caad on 2016/12/26.
+//  Copyright © 2016年 LD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+typedef NS_OPTIONS(NSInteger,Borders)
+{
+    TopBorder       = 1 << 0,   // 上边框
+    LeftBorder      = 1 << 1,   // 左边框
+    RightBorder     = 1 << 2,   // 右边框
+    BottomBorder    = 1 << 3,   // 下边框
+    AllBorders      = (TopBorder | LeftBorder | RightBorder | BottomBorder) // 上下左右边框
+};
+
+@interface UIView (Border)
+/** @brief 给View的顶部添加内边框,边框两边与View对齐
+ *
+ * @param color 边框的颜色
+ * @param height 边框的高度
+ */
+- (void)addTopBorderWithColor:(UIColor *)color height:(CGFloat)height;
+/** 给View的左边添加内边框,边框顶部和底部与View对齐 
+ *
+ * @param color 边框的颜色
+ * @param width 变宽的宽度
+ */
+- (void)addLeftBorderWithColor:(UIColor *)color width:(CGFloat)width;
+/** 给View的右边添加内边框,边框顶部和底部与View对齐 
+ *
+ * @param color 边框的颜色
+ * @param width 变宽的宽度
+ */
+- (void)addRightBorderWithColor:(UIColor *)color width:(CGFloat)width;
+/**
+ * 给View的底部添加内边框,边框两边与View对齐
+ *
+ * @param color 边框的颜色
+ * @param height 底部边框的高度
+ */
+-(void)addBottomBorderWithColor:(UIColor *)color height:(CGFloat)height;
+/** 给View的顶部添加内边框
+ *
+ * @param color 边框的颜色
+ * @param height 边框的高度
+ * @param leading 距离View左边的距离
+ * @param trailing 距离View右边的距离
+ */
+- (void)addTopBorderWithColor:(UIColor *)color height:(CGFloat)height leading:(CGFloat)leading trailing:(CGFloat)trailing;
+/** 给View的左边添加内边框
+ *
+ * @param color 边框的颜色
+ * @param width 边框的高度
+ * @param top 距离View顶部的距离
+ * @param bottom 距离View底部的距离
+ */
+- (void)addLeftBorderWithColor:(UIColor *)color width:(CGFloat)width top:(CGFloat)top bottom:(CGFloat)bottom;
+/** 给View的右边添加内边框
+ *
+ * @param color 边框的颜色
+ * @param width 边框的高度
+ * @param top 距离View顶部的距离
+ * @param bottom 距离View底部的距离
+ */
+- (void)addRightBorderWithColor:(UIColor *)color width:(CGFloat)width top:(CGFloat)top bottom:(CGFloat)bottom;
+/** 给View的底部添加内边框
+ *
+ * @param color 边框的颜色
+ * @param height 边框的高度
+ * @param leading 距离View左边的距离
+ * @param trailing 距离View右边的距离
+ */
+- (void)addBottomBorderWithColor:(UIColor *)color height:(CGFloat)height leading:(CGFloat)leading trailing:(CGFloat)trailing;
+/**
+ *  @brief 给View添加内边框
+ *
+ *  @param borders 需要添加边框的方向(NS_OPTIONS)
+ *  @param color 边框的颜色
+ *  @param width 边框的宽度
+ */
+- (void)addBorders:(Borders)borders color:(UIColor*)color width:(CGFloat)width;
+
+@end
---
+++ UIView+Border.m
@@ -0,0 +1,78 @@
+//
+//  UIView+Border.m
+//  Layout
+//
+//  Created by YB on 2016/12/26.
+//  Copyright © 2016年 LD. All rights reserved.
+//
+
+#import "UIView+Border.h"
+
+@implementation UIView (Border)
+
+- (void)addTopBorderWithColor:(UIColor *)color height:(CGFloat)height
+{
+    [self addTopBorderWithColor:color height:height leading:0 trailing:0];
+}
+- (void)addLeftBorderWithColor:(UIColor *)color width:(CGFloat)width
+{
+    [self addLeftBorderWithColor:color width:width top:0 bottom:0];
+}
+- (void)addRightBorderWithColor:(UIColor *)color width:(CGFloat)width
+{
+    [self addRightBorderWithColor:color width:width top:0 bottom:0];
+}
+- (void)addBottomBorderWithColor:(UIColor *)color height:(CGFloat)height
+{
+    [self addBottomBorderWithColor:color height:height leading:0 trailing:0];
+}
+
+- (void)addTopBorderWithColor:(UIColor *)color height:(CGFloat)height leading:(CGFloat)leading trailing:(CGFloat)trailing
+{
+    CALayer *topLayer = [[CALayer alloc] init];
+    topLayer.frame = CGRectMake(leading, 0, self.frame.size.width-leading-trailing, height);
+    topLayer.backgroundColor = color.CGColor;
+    [self.layer addSublayer:topLayer];
+}
+- (void)addLeftBorderWithColor:(UIColor *)color width:(CGFloat)width top:(CGFloat)top bottom:(CGFloat)bottom
+{
+    CALayer *leftLayer = [[CALayer alloc] init];
+    leftLayer.frame = CGRectMake(0, top, width, self.frame.size.height-top-bottom);
+    leftLayer.backgroundColor = color.CGColor;
+    [self.layer addSublayer:leftLayer];
+}
+- (void)addRightBorderWithColor:(UIColor *)color width:(CGFloat)width top:(CGFloat)top bottom:(CGFloat)bottom
+{
+    CALayer *rightLayer = [[CALayer alloc] init];
+    rightLayer.frame = CGRectMake(self.frame.size.width-width, top, width, self.frame.size.height-top-bottom);
+    rightLayer.backgroundColor = color.CGColor;
+    [self.layer addSublayer:rightLayer];
+}
+- (void)addBottomBorderWithColor:(UIColor *)color height:(CGFloat)height leading:(CGFloat)leading trailing:(CGFloat)trailing
+{
+    CALayer *bottomLayer = [[CALayer alloc] init];
+    bottomLayer.frame = CGRectMake(leading, self.frame.size.height - height, self.frame.size.width-leading-trailing, height);
+    bottomLayer.backgroundColor = color.CGColor;
+    [self.layer addSublayer:bottomLayer];
+}
+
+- (void)addBorders:(Borders)borders color:(UIColor*)color width:(CGFloat)width;
+{
+    if (borders & TopBorder)
+    {
+        [self addTopBorderWithColor:color height:width];
+    }
+    if (borders & LeftBorder)
+    {
+        [self addLeftBorderWithColor:color width:width];
+    }
+    if (borders & RightBorder)
+    {
+        [self addRightBorderWithColor:color width:width];
+    }
+    if (borders & BottomBorder)
+    {
+        [self addBottomBorderWithColor:color height:width];
+    }
+}
+@end
